9d86a39e5cf9fa1218b41e2d2692ff31bcf4b895
Agustinus Theodorus <agustinustheoo@gmail.com>
2023-10-27 08:38:43 +0700

    feat: print .env file for api and web directories

--- windows/InstallMyriadAndNgrok.ps1
+++ windows/InstallMyriadAndNgrok.ps1
@@ -110,10 +110,98 @@ if (-not (Test-Path $myriadApi) -or -not (Test-Path $myriadWeb)) {
     Write-Host "Repositories cloned successfully in the 'myriad' directory!"
 }
 
-# Navigate to your project directory (replace with your actual path)
-$projectPath = "D:\myriad\myriad-api"
+Set-Location -Path $myriadWeb
 
-cd $projectPath
+$envContent = @"
+NODE_ENV=development
+HOST=127.0.0.1
+PORT=3000
+
+# APP
+NEXT_PUBLIC_APP_ENVIRONMENT=testnet
+NEXT_PUBLIC_APP_NAME=Myriad App
+NEXT_PUBLIC_APP_VERSION=2.1.20
+NEXT_PUBLIC_APP_AUTH_URL=http://localhost:3000
+APP_SECRET=crOuQ926z0PGP2u4zUC624775d4diXB4
+
+# MYRIAD
+NEXT_PUBLIC_MYRIAD_SUPPORT_MAIL=support@myriad.social
+NEXT_PUBLIC_MYRIAD_WEBSITE_URL=https://myriad.social
+NEXT_PUBLIC_MYRIAD_RPC_URL=wss://ws-rpc.devnet.myriad.social
+NEXT_PUBLIC_MYRIAD_API_URL=http://localhost:4000
+
+# TIPPING CONTRACT
+NEAR_TIPPING_CONTRACT_ID=myriadtips.testnet
+
+# FIREBASE
+NEXT_PUBLIC_FIREBASE_PROJECT_ID=
+NEXT_PUBLIC_FIREBASE_API_KEY=
+NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID=
+NEXT_PUBLIC_FIREBASE_APP_ID=
+NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID=
+NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET=
+
+# SENTRY
+NEXT_PUBLIC_SENTRY_DSN=
+"@
+
+$envContent | Out-File .env -Encoding utf8
+
+
+# Navigate to your project directory (replace with your actual path)
+Set-Location -Path $myriadApi
+
+$envContent = @"
+DOMAIN=localhost
+
+# MYRIAD
+MYRIAD_ADMIN_SUBSTRATE_MNEMONIC=
+MYRIAD_ADMIN_NEAR_MNEMONIC=
+
+# JWT
+JWT_TOKEN_SECRET_KEY=jr6AnWfTcUkQH768
+JWT_TOKEN_EXPIRES_IN=36000
+JWT_REFRESH_TOKEN_SECRET_KEY=Y2csOsu072uGB5UV
+JWT_REFRESH_TOKEN_EXPIRES_IN=216000
+
+# MONGO
+MONGO_PROTOCOL=mongodb
+MONGO_HOST=localhost
+MONGO_PORT=27017
+MONGO_USER=
+MONGO_PASSWORD=
+MONGO_DATABASE=myriad
+MONGO_URL=mongodb://localhost:27017/myriad?replicaSet=rs0&w=majority&wtimeoutMS=2000
+
+# REDIS
+REDIS_CONNECTOR=
+REDIS_HOST=
+REDIS_PORT=
+REDIS_PASSWORD=
+
+# SMTP
+SMTP_SERVER=
+SMTP_PORT=
+SMTP_USERNAME=
+SMTP_PASSWORD=
+SMTP_SENDER_ADDRESS=
+
+# FIREBASE
+# Path to sevice account credential file, e.g /etc/gcp/sa_credentials.json
+GOOGLE_APPLICATION_CREDENTIALS=
+FIREBASE_STORAGE_BUCKET=
+
+# SENTRY
+SENTRY_DSN=
+
+# TWITTER
+TWITTER_API_KEY=
+
+# COIN MARKET CAP
+COIN_MARKET_CAP_API_KEY=
+"@
+
+$envContent | Out-File .env -Encoding utf8
 
 # Install dependencies as per your project's requirements
 yarn install --frozen-lockfile --ignore-engines --network-timeout 100000
